5aaf43515879a29f4fc69b383e56c0b3ddf9210e
Thibaut Séguy <thibaut.seguy@dataptive.io>
2020-11-30 12:43:42 +0100

    add backup and restore

--- log/log.go
+++ log/log.go
@@ -1,7 +1,10 @@
 package log
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"errors"
+	"io"
 	"os"
 	"path/filepath"
 	"sync"
@@ -161,6 +164,73 @@ func Delete(path string) (err error) {
 	return nil
 }
 
+func Restore(path string, r io.Reader) (err error) {
+
+	err = os.Mkdir(path, os.FileMode(dirPerm))
+	if err != nil {
+		if os.IsExist(err) {
+			return ErrExist
+		}
+
+		return err
+	}
+
+	parentDirname := filepath.Dir(path)
+
+	err = syncDirectory(parentDirname)
+	if err != nil {
+		panic(err)
+	}
+
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+
+	tr := tar.NewReader(gzr)
+
+	for {
+		header, err := tr.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		pathname := filepath.Join(path, header.Name)
+
+		f, err := os.OpenFile(pathname, os.O_WRONLY|os.O_CREATE, os.FileMode(header.Mode))
+		if err != nil {
+			return err
+		}
+
+		err = syncDirectory(path)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = io.Copy(f, tr)
+		if err != nil {
+			return err
+		}
+
+		err = f.Sync()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = gzr.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func newLog(path string, config Config, options Options) (l *Log, err error) {
 
 	l = &Log{
@@ -277,6 +347,257 @@ func (l *Log) Unsubscribe(subscriber chan LogInfo) {
 
 	l.subscribers[pos] = l.subscribers[len(l.subscribers)-1]
 	l.subscribers = l.subscribers[:len(l.subscribers)-1]
+}
+
+func (l *Log) Backup(w io.Writer) (err error) {
+
+	// Checkpoint current log state.
+	logInfo := l.Stat()
+
+	// Build a list a index and records file handles.
+	names, err := listSegments(l.path)
+	if err != nil {
+		return err
+	}
+
+	var recordsFiles []*os.File
+	var indexFiles []*os.File
+
+	for _, name := range names {
+
+		pathname := filepath.Join(l.path, name)
+
+		f, err := os.Open(pathname + recordsSuffix)
+		if err != nil {
+			return err
+		}
+
+		recordsFiles = append(recordsFiles, f)
+
+		f, err = os.Open(pathname + indexSuffix)
+		if err != nil {
+			return err
+		}
+
+		indexFiles = append(indexFiles, f)
+	}
+
+	// Get a config file handle.
+	configPathname := filepath.Join(l.path, configFilename)
+
+	configFile, err := os.Open(configPathname)
+	if err != nil {
+		return err
+	}
+
+	// Prepare the tar gz writer.
+	gzw := gzip.NewWriter(w)
+	tw := tar.NewWriter(gzw)
+
+	// Add the config file to the archive.
+	fi, err := configFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := tar.FileInfoHeader(fi, fi.Name())
+	if err != nil {
+		return err
+	}
+
+	err = tw.WriteHeader(header)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(tw, configFile)
+	if err != nil {
+		return err
+	}
+
+	err = configFile.Close()
+	if err != nil {
+		return err
+	}
+
+	// Save the last records file to process it separately.
+	lastRecordsFile := recordsFiles[len(recordsFiles)-1]
+
+	// Add all records files except the last one to the archive.
+	recordsFiles = recordsFiles[:len(recordsFiles)-1]
+
+	for _, recordsFile := range recordsFiles {
+
+		fi, err := recordsFile.Stat()
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(fi, fi.Name())
+		if err != nil {
+			return err
+		}
+
+		err = tw.WriteHeader(header)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(tw, recordsFile)
+		if err != nil {
+			return err
+		}
+
+		err = recordsFile.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	// Copy the last records file to the archive, up to the checkpointed
+	// offset.
+	fi, err = lastRecordsFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	filename := fi.Name()
+	segmentName := filename[:len(filename)-len(recordsSuffix)]
+	_, baseOffset, _ := parseSegmentName(segmentName)
+
+	header = &tar.Header{
+		Name: fi.Name(),
+		Mode: int64(fi.Mode().Perm()),
+		Size: logInfo.EndOffset - baseOffset,
+	}
+
+	err = tw.WriteHeader(header)
+	if err != nil {
+		return err
+	}
+
+	lr := &io.LimitedReader{
+		R: lastRecordsFile,
+		N: header.Size,
+	}
+
+	_, err = io.Copy(tw, lr)
+	if err != nil {
+		return err
+	}
+
+	err = lastRecordsFile.Close()
+	if err != nil {
+		return err
+	}
+
+	// Save the last index file to process it separately.
+	lastIndexFile := indexFiles[len(indexFiles)-1]
+
+	// Add all index files except the last one to the archive.
+	indexFiles = indexFiles[:len(indexFiles)-1]
+
+	for _, indexFile := range indexFiles {
+
+		fi, err := indexFile.Stat()
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(fi, fi.Name())
+		if err != nil {
+			return err
+		}
+
+		err = tw.WriteHeader(header)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(tw, indexFile)
+		if err != nil {
+			return err
+		}
+
+		err = indexFile.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	// Find the last index entry matching the checkpointed state and copy
+	// the last index file up to this point.
+	indexBufferedReader := recio.NewBufferedReader(lastIndexFile, 1 << 20, recio.ModeAuto)
+	indexAtomicReader := recio.NewAtomicReader(indexBufferedReader)
+
+	ie := indexEntry{}
+
+	offset := int64(0)
+	for {
+		n, err := indexAtomicReader.Read(&ie)
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		if ie.position > logInfo.EndOffset {
+			break
+		}
+
+		offset += int64(n)
+	}
+
+	fi, err = lastIndexFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	header = &tar.Header{
+		Name: fi.Name(),
+		Mode: int64(fi.Mode().Perm()),
+		Size: offset,
+	}
+
+	err = tw.WriteHeader(header)
+	if err != nil {
+		return err
+	}
+
+	_, err = lastIndexFile.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return err
+	}
+
+	lr = &io.LimitedReader{
+		R: lastIndexFile,
+		N: header.Size,
+	}
+
+	_, err = io.Copy(tw, lr)
+	if err != nil {
+		return err
+	}
+
+	err = lastIndexFile.Close()
+	if err != nil {
+		return err
+	}
+
+	err = tw.Close()
+	if err != nil {
+		return err
+	}
+
+	err = gzw.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (l *Log) initializeStat() (err error) {
